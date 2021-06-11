a72fca13b2779372883db1b6cd603d084eddb609
Yihong Sun <yihongs@node031.ib.cluster>
2021-06-11 17:10:03 -0400

    added argparse

--- wget_gdrive.py
+++ wget_gdrive.py
@@ -1,28 +1,34 @@
-import sys, os
+import sys, os, argparse
 from subprocess import call
+from datetime import datetime
 
-try:
-    link = sys.argv[1]
-    FILENAME = sys.argv[2]
-    print('Link: {}\nFile Name: {}'.format(link, FILENAME))
-except:
-    print('Please enter link and desired file name as a command link argument.')
+parser = argparse.ArgumentParser()
+parser.add_argument('--link', default=None, 
+    help="Google Drive Link")
+args = parser.parse_args(sys.argv[1:])
 
-link_parts = link.split('/')
+if args.link == None:
+    print('Please enter the gdrive download link: \'python3 wget_drive.py --link=<gdrive download link>')
+
+link_parts = args.link.split('/')
 
 try:
     gcom_idx = link_parts.index('drive.google.com')
-    assert gcom_idx > -1 and link_parts[gcom_idx+2] == 'd' and link_parts[gcom_idx+4] == 'view?usp=sharing'
+    assert gcom_idx > -1 and link_parts[gcom_idx+2] == 'd'
     FILEID = link_parts[gcom_idx+3]
 except:
     print('Error: Invalid Link.')
 
+savename = '{}'.format(datetime.now().strftime("%Y_%m_%d__%H_%M"))
+
 bash_file = open('./tmp.sh', 'w')
 bash_file.write("#!/bin/sh\n")
-bash_file.write("wget --load-cookies /tmp/cookies.txt \"https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id={0}' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\\1\\n/p')&id={0}\" -O {1} && rm -rf /tmp/cookies.txt\n".format(FILEID, FILENAME))
+bash_file.write("wget --load-cookies /tmp/cookies.txt \"https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id={0}' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\\1\\n/p')&id={0}\" -O {1} && rm -rf /tmp/cookies.txt\n".format(FILEID, savename))
 bash_file.close()
 
 os.chmod('./tmp.sh', 0o744)
 call('./tmp.sh')
 
 os.remove('./tmp.sh')
+
+print('Download Completed! File saved under \'{}\''.format(savename))
